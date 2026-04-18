f0b56de5ff8a257a5171271dbdc45fdf9be439e0
holgerengels <hengels@gmail.com>
2026-04-18 08:44:12 +0200

    Update remote to use continuous RC commands

--- remote.js
+++ remote.js
@@ -6,7 +6,7 @@ async function run() {
     const drone = new Tello();
 
     try {
-        console.log('--- Tello Remote Control ---');
+        console.log('--- Tello Remote Control (Continuous Mode) ---');
         console.log('Stelle sicher, dass du mit dem WLAN der Drohne verbunden bist.');
         await drone.init();
         console.log('\n[Drohne ist initialisiert (SDK Modus)]');
@@ -17,27 +17,79 @@ async function run() {
             console.log(`- [${k}]: ${keys[k]}`);
         });
         console.log('- [ctrl+c]: Beenden\n');
-
-        console.log('Warte auf Eingabe...');
+        
+        console.log('DRÜCKE "SPACE" UM ANZUSCHWEBEN / ANZUHALTEN (STOP)!\n');
 
         readline.emitKeypressEvents(process.stdin);
         if (process.stdin.isTTY) {
             process.stdin.setRawMode(true);
         }
 
+        let rcState = { a: 0, b: 0, c: 0, d: 0 };
+        const speed = 50; // Geschwindigkeit der Bewegung (10 bis 100)
+
+        // rc command lookup (action -> axis and velocity change)
+        const rcActionMap = {
+            'forward': { axis: 'b', val: speed },
+            'back': { axis: 'b', val: -speed },
+            'left': { axis: 'a', val: -speed }, // seitwärts links
+            'right': { axis: 'a', val: speed }, // seitwärts rechts
+            'up': { axis: 'c', val: speed },
+            'down': { axis: 'c', val: -speed },
+            'ccw': { axis: 'd', val: -speed }, // drehen links herum
+            'cw': { axis: 'd', val: speed }    // drehen rechts herum
+        };
+
+        let rcInterval = null;
+
+        const sendRcCommand = () => {
+            // rc a b c d
+            // a: links/rechts, b: vor/zurück, c: hoch/runter, d: yaw (drehung)
+            drone.send(`rc ${rcState.a} ${rcState.b} ${rcState.c} ${rcState.d}`).catch(() => {});
+        };
+
         process.stdin.on('keypress', async (str, key) => {
-            // Beenden bei ctrl+c
             if (key.ctrl && key.name === 'c') {
                 console.log('\nBeende...');
+                if (rcInterval) clearInterval(rcInterval);
                 drone.close();
                 process.exit();
             }
 
             const command = keys[key.name];
-            if (command) {
-                // Wir nutzen hier Fire & Forget, da wir bei einer manuellen Steuerung 
-                // nicht zwingend auf das Ende einer Bewegung warten wollen, bis wir
-                // z.B. einen Stop-Befehl absetzen.
+            if (!command) return;
+
+            if (command === 'stop') {
+                // Alle RC werte auf 0 zurücksetzen und anhalten
+                rcState = { a: 0, b: 0, c: 0, d: 0 };
+                if (rcInterval) {
+                    clearInterval(rcInterval);
+                    rcInterval = null;
+                }
+                
+                // Explizit den Hover-Befehl senden, um sofort anzuhalten
+                drone.send('rc 0 0 0 0').catch(()=>{}); 
+                drone.send('stop').catch(()=>{}); 
+                console.log('\n[STOP] Drohne schwebt auf der Stelle.');
+                
+            } else if (command.startsWith('rc:')) {
+                const action = command.split(':')[1];
+                const rcDef = rcActionMap[action];
+                
+                if (rcDef) {
+                    // Werte updaten
+                    rcState[rcDef.axis] = rcDef.val;
+                    console.log(`[RC Update] Axis '${rcDef.axis}' = ${rcDef.val} (a:${rcState.a} b:${rcState.b} c:${rcState.c} d:${rcState.d})`);
+                    
+                    // Sende Tello den neuen Status kontinuierlich, da der SDK Modus oft laufende Updates für RC erwartet
+                    if (!rcInterval) {
+                        sendRcCommand();
+                        rcInterval = setInterval(sendRcCommand, 200); // alle 200ms senden
+                    }
+                }
+            } else {
+                // Normale einmalige Befehle wie takeoff, land
+                console.log(`\n[Befehl ausgelöst]: ${command}`);
                 drone.send(command).catch(e => {
                     console.error(`Fehler beim Senden von '${command}':`, e);
                 });
